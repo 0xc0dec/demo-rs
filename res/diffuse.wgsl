1476f582660e483a707f72e935d6dac712090568
Aleksey Fedotov <aleksey.fedotov@gmail.com>
2023-03-02 21:49:55 +0500

    Add basic model matrix to shader

--- res/diffuse.wgsl
+++ res/diffuse.wgsl
@@ -1,11 +1,12 @@
 // Vertex shader
 
-struct CameraUniform {
+struct MatricesUniform {
     view_proj: mat4x4<f32>,
+    world: mat4x4<f32>
 };
 
 @group(1) @binding(0)
-var<uniform> camera: CameraUniform;
+var<uniform> matrices: MatricesUniform;
 
 struct VertexInput {
     @location(0) position: vec3<f32>,
@@ -21,7 +22,7 @@ struct VertexOutput {
 fn vs_main(model: VertexInput) -> VertexOutput {
     var out: VertexOutput;
     out.tex_coords = model.tex_coords;
-    out.clip_position = camera.view_proj * vec4<f32>(model.position, 1.0);
+    out.clip_position = matrices.view_proj * matrices.world * vec4<f32>(model.position, 1.0);
     return out;
 }
 
--- src/material.rs
+++ src/material.rs
@@ -1,4 +1,4 @@
-use cgmath::Matrix4;
+use cgmath::{Deg, Matrix4, Rad, Vector3};
 use wgpu::{BindGroup, RenderPass, RenderPipeline};
 use wgpu::util::DeviceExt;
 use crate::camera::Camera;
@@ -6,6 +6,7 @@ use crate::model::{ModelVertex, Vertex};
 use crate::renderer::Renderer;
 use crate::resources::load_string;
 use crate::texture::Texture;
+use crate::transform::{Transform, TransformSpace};
 
 #[rustfmt::skip]
 pub const OPENGL_TO_WGPU_MATRIX: Matrix4<f32> = Matrix4::new(
@@ -17,28 +18,31 @@ pub const OPENGL_TO_WGPU_MATRIX: Matrix4<f32> = Matrix4::new(
 
 #[repr(C)]
 #[derive(Debug, Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
-struct CameraUniform {
+struct MatricesUniform {
     view_proj: [[f32; 4]; 4],
+    world: [[f32; 4]; 4]
 }
 
-impl CameraUniform {
+impl MatricesUniform {
     fn new() -> Self {
         use cgmath::SquareMatrix;
         Self {
             view_proj: Matrix4::identity().into(),
+            world: Matrix4::identity().into()
         }
     }
 
-    fn update_view_proj(&mut self, camera: &Camera) {
+    fn update(&mut self, camera: &Camera, model_transform: &Transform) {
         self.view_proj = (OPENGL_TO_WGPU_MATRIX * camera.view_proj_matrix()).into();
+        self.world = model_transform.matrix().into();
     }
 }
 
 pub struct Material {
     texture_bind_group: BindGroup,
-    camera_uniform: CameraUniform,
-    camera_buffer: wgpu::Buffer,
-    camera_bind_group: BindGroup,
+    matrices_uniform: MatricesUniform,
+    matrices_uniform_buf: wgpu::Buffer,
+    matrices_uniform_bind_group: BindGroup,
     render_pipeline: RenderPipeline,
 }
 
@@ -93,17 +97,17 @@ impl Material {
             label: None,
         });
 
-        let camera_uniform = CameraUniform::new();
+        let matrices_uniform = MatricesUniform::new();
 
-        let camera_buffer = renderer.device().create_buffer_init(
+        let matrices_uniform_buf = renderer.device().create_buffer_init(
             &wgpu::util::BufferInitDescriptor {
                 label: None,
-                contents: bytemuck::cast_slice(&[camera_uniform]),
+                contents: bytemuck::cast_slice(&[matrices_uniform]),
                 usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
             }
         );
 
-        let camera_bind_group_layout = renderer.device().create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+        let matrices_uniform_bind_group_layout = renderer.device().create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
             entries: &[
                 wgpu::BindGroupLayoutEntry {
                     binding: 0,
@@ -119,12 +123,12 @@ impl Material {
             label: None,
         });
 
-        let camera_bind_group = renderer.device().create_bind_group(&wgpu::BindGroupDescriptor {
-            layout: &camera_bind_group_layout,
+        let matrices_uniform_bind_group = renderer.device().create_bind_group(&wgpu::BindGroupDescriptor {
+            layout: &matrices_uniform_bind_group_layout,
             entries: &[
                 wgpu::BindGroupEntry {
                     binding: 0,
-                    resource: camera_buffer.as_entire_binding(),
+                    resource: matrices_uniform_buf.as_entire_binding(),
                 }
             ],
             label: None,
@@ -134,7 +138,7 @@ impl Material {
             label: None,
             bind_group_layouts: &[
                 &texture_bind_group_layout,
-                &camera_bind_group_layout
+                &matrices_uniform_bind_group_layout
             ],
             push_constant_ranges: &[]
         });
@@ -182,9 +186,9 @@ impl Material {
 
         Self {
             texture_bind_group,
-            camera_uniform,
-            camera_buffer,
-            camera_bind_group,
+            matrices_uniform,
+            matrices_uniform_buf,
+            matrices_uniform_bind_group,
             render_pipeline,
         }
     }
@@ -196,15 +200,18 @@ pub trait RenderMaterial<'a> {
 
 impl<'a, 'b> RenderMaterial<'b> for RenderPass<'a> where 'b: 'a {
     fn apply_material(&mut self, renderer: &'b Renderer, material: &'b mut Material, camera: &'b Camera) {
-        material.camera_uniform.update_view_proj(camera);
+        let mut transform = Transform::new();
+        transform.rotate_around_axis(Vector3::unit_z(), Rad::from(Deg(45.0)), TransformSpace::World);
+        material.matrices_uniform.update(camera, &transform);
+
         renderer.queue().write_buffer(
-            &material.camera_buffer,
+            &material.matrices_uniform_buf,
             0,
-            bytemuck::cast_slice(&[material.camera_uniform])
+            bytemuck::cast_slice(&[material.matrices_uniform])
         );
 
         self.set_pipeline(&material.render_pipeline);
         self.set_bind_group(0, &material.texture_bind_group, &[]);
-        self.set_bind_group(1, &material.camera_bind_group, &[]);
+        self.set_bind_group(1, &material.matrices_uniform_bind_group, &[]);
     }
 }
