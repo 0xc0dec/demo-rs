201036c1ecf12b72016d0d6158237f4057284e48
Aleksey Fedotov <aleksey.fedotov@gmail.com>
2023-03-08 15:33:51 +0500

    Add dt filtering and set window size

--- src/main.rs
+++ src/main.rs
@@ -10,19 +10,29 @@ mod materials;
 mod scene;
 mod physics;
 
+use std::collections::VecDeque;
 use winit::{event::*, event_loop::{ControlFlow, EventLoop}, window::WindowBuilder};
+use winit::dpi::{PhysicalSize, Size};
+use winit::platform::run_return::EventLoopExtRunReturn;
 
 use input::Input;
 use graphics::Graphics;
 use render_target::RenderTarget;
 use crate::scene::Scene;
 
+const DT_FILTER_WIDTH: usize = 10;
+
 async fn run() {
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new()
         .with_title("Demo")
+        .with_inner_size(PhysicalSize::new(1600, 1200))
         .build(&event_loop)
         .unwrap();
+
+    let mut dt_queue: VecDeque<f32> = VecDeque::with_capacity(DT_FILTER_WIDTH);
+    let mut fake_dt_countdown: i32 = 1;
+    let mut last_frame_instant = std::time::Instant::now();
 
     let mut gfx = Graphics::new(&window).await;
     let mut render_target = RenderTarget::new(&gfx, wgpu::Color {
@@ -33,7 +43,6 @@ async fn run() {
     });
     let mut scene = Scene::new(&gfx).await;
     let mut input = Input::new();
-    let mut time = instant::Instant::now();
 
     event_loop.run(move |event, _, flow| {
         *flow = ControlFlow::Poll;
@@ -65,10 +74,35 @@ async fn run() {
             }
 
             Event::MainEventsCleared => {
-                let dt = instant::Instant::now() - time;
-                time = instant::Instant::now();
+                // let dt = instant::Instant::now() - time;
+                // time = instant::Instant::now();
 
-                scene.update(&input, dt.as_secs_f32());
+                // Stolen from Kajiya
+                let dt_filtered = {
+                    let now = std::time::Instant::now();
+                    let dt_duration = now - last_frame_instant;
+                    last_frame_instant = now;
+
+                    let dt_raw = dt_duration.as_secs_f32();
+
+                    // >= because rendering (and thus the spike) happens _after_ this.
+                    if fake_dt_countdown >= 0 {
+                        // First frame. Return the fake value.
+                        fake_dt_countdown -= 1;
+                        dt_raw.min(1.0 / 60.0)
+                    } else {
+                        // Not the first frame. Start averaging.
+
+                        if dt_queue.len() >= DT_FILTER_WIDTH {
+                            dt_queue.pop_front();
+                        }
+
+                        dt_queue.push_back(dt_raw);
+                        dt_queue.iter().copied().sum::<f32>() / dt_queue.len() as f32
+                    }
+                };
+
+                scene.update(&input, dt_filtered);
 
                 gfx.render_frame(&mut scene, &render_target);
 
