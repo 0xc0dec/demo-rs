77db1891716717c9f08515d816ec26a9e4d32e46
Aleksey Fedotov <aleksey.fedotov@gmail.com>
2023-03-24 23:57:22 +0500

    Fix compilation

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -2,7 +2,9 @@ mod player;
 mod camera;
 mod skybox;
 mod spectator;
+mod physics_box;
 
 pub use camera::Camera;
 pub use player::Player;
 pub use skybox::Skybox;
+pub use physics_box::{PhysicsBox, PhysicsBoxParams};
