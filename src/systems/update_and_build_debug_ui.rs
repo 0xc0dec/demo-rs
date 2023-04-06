6e9b95504187632c97dcf91a093173d304b6631c
Aleksey Fedotov <aleksey.fedotov@gmail.com>
2023-04-06 10:31:43 +0500

    Update screenshot and debug UI

--- src/systems/update_and_build_debug_ui.rs
+++ src/systems/update_and_build_debug_ui.rs
@@ -34,6 +34,7 @@ pub fn update_and_build_debug_ui(
                     * Right mouse button to control camera\n\
                     * Q-W-E-A-S-D to move\n\
                     * Left mouse button to grab and release an object\n\
+                    * Space to spawn a new box\n\
                     * Esc to quit the app",
                 );
 
