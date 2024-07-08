6d85a6b0f134dc6d87e52312cd1f8d50ea01788d
Aleksey Fedotov <aleksey.fedotov@gmail.com>
2024-07-08 20:22:45 +0500

    Extract function

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use winit::dpi::PhysicalSize;
 use winit::event::{DeviceEvent, ElementState, Event, KeyboardInput, WindowEvent};
 use winit::event_loop::{ControlFlow, EventLoop};
 use winit::platform::run_return::EventLoopExtRunReturn;
-use winit::window::WindowBuilder;
+use winit::window::{Window, WindowBuilder};
 
 use crate::resources::*;
 
@@ -16,6 +16,73 @@ mod resources;
 mod systems;
 
 // TODO After removing Bevy, refactor file structure, remove the notion of components/resources/systems.
+
+fn consume_system_events(
+    event_loop: &mut EventLoop<()>,
+    window: &Window,
+    mouse_events: &mut Vec<MouseEvent>,
+    keyboard_events: &mut Vec<KeyboardEvent>,
+    resize_events: &mut Vec<ResizeEvent>,
+) {
+    event_loop.run_return(|event, _, flow| {
+        *flow = ControlFlow::Poll;
+
+        match event {
+            Event::MainEventsCleared => {
+                *flow = ControlFlow::Exit;
+            }
+
+            Event::DeviceEvent {
+                event: DeviceEvent::MouseMotion { delta },
+                ..
+            } => {
+                mouse_events.push(MouseEvent::Move {
+                    dx: delta.0 as f32,
+                    dy: delta.1 as f32,
+                });
+            }
+
+            Event::WindowEvent {
+                ref event,
+                window_id,
+            } if window_id == window.id() => match event {
+                WindowEvent::MouseInput { state, button, .. } => {
+                    mouse_events.push(MouseEvent::Button {
+                        btn: *button,
+                        pressed: *state == ElementState::Pressed,
+                    });
+                }
+
+                WindowEvent::KeyboardInput {
+                    input:
+                        KeyboardInput {
+                            state: key_state,
+                            virtual_keycode: Some(keycode),
+                            ..
+                        },
+                    ..
+                } => {
+                    keyboard_events.push(KeyboardEvent {
+                        code: *keycode,
+                        pressed: *key_state == ElementState::Pressed,
+                    });
+                }
+
+                WindowEvent::Resized(new_size) => {
+                    resize_events.push(ResizeEvent(*new_size));
+                }
+
+                WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                    resize_events.push(ResizeEvent(**new_inner_size));
+                }
+
+                _ => (),
+            },
+
+            _ => {}
+        }
+    });
+}
 
 fn main() {
     let mut event_loop = EventLoop::new();
@@ -32,72 +99,19 @@ fn main() {
     let mut input = Input::new();
     let mut frame_time = FrameTime::new();
 
-    // TODO More optimal, this is an MVP
+    // TODO More optimal, avoid vec cleanup on each iteration
     let mut mouse_events = Vec::new();
     let mut keyboard_events = Vec::new();
     let mut resize_events = Vec::new();
 
     while !input.action_active(InputAction::Escape) {
-        event_loop.run_return(|event, _, flow| {
-            *flow = ControlFlow::Poll;
-
-            match event {
-                Event::MainEventsCleared => {
-                    *flow = ControlFlow::Exit;
-                }
-
-                Event::DeviceEvent {
-                    event: DeviceEvent::MouseMotion { delta },
-                    ..
-                } => {
-                    mouse_events.push(MouseEvent::Move {
-                        dx: delta.0 as f32,
-                        dy: delta.1 as f32,
-                    });
-                }
-
-                Event::WindowEvent {
-                    ref event,
-                    window_id,
-                } if window_id == window.id() => match event {
-                    WindowEvent::MouseInput { state, button, .. } => {
-                        mouse_events.push(MouseEvent::Button {
-                            btn: *button,
-                            pressed: *state == ElementState::Pressed,
-                        });
-                    }
-
-                    WindowEvent::KeyboardInput {
-                        input:
-                            KeyboardInput {
-                                state: key_state,
-                                virtual_keycode: Some(keycode),
-                                ..
-                            },
-                        ..
-                    } => {
-                        keyboard_events.push(KeyboardEvent {
-                            code: *keycode,
-                            pressed: *key_state == ElementState::Pressed,
-                        });
-                    }
-
-                    WindowEvent::Resized(new_size) => {
-                        resize_events.push(ResizeEvent(*new_size));
-                    }
-
-                    WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
-                        resize_events.push(ResizeEvent(**new_inner_size));
-                    }
-
-                    _ => (),
-                },
-
-                _ => {}
-            }
-
-            // debug_ui.handle_window_event(&window, &event);
-        });
+        consume_system_events(
+            &mut event_loop,
+            &window,
+            &mut mouse_events,
+            &mut keyboard_events,
+            &mut resize_events,
+        );
 
         if let Some(e) = resize_events.last() {
             device.resize(e.0);
